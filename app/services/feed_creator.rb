7bd6b223d4df02cee66f5f9626bb091857c82ef4
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2019-09-05 22:07:36 +0300

    Better error handling.

--- app/services/feed_creator.rb
+++ app/services/feed_creator.rb
@@ -18,15 +18,21 @@ class FeedCreator
     return self unless params_valid?
 
     response = HTTPX.get(@url)
-    (errors.messages[:feed_request] = response.to_s) && (return self) if response.status > 205
+    if response.status > 200
+      errors.messages[:feed_request] = { response: response.to_s, status: response.status }
+      return self
+    end
 
     fetched_feed = Feedjira.parse(response.to_s)
 
     @feed = Feed.new(@params)
     @feed.title = fetched_feed.title
 
-    @feed.save
+    @feed.save! if @feed.valid?
     @feed
+  rescue StandardError => e
+    errors.messages[:exception] = e
+    self
   end
 
   private
@@ -36,5 +42,6 @@ class FeedCreator
     return true if Twingly::URL.parse(@url).valid?
 
     errors.messages[:url] = "#{@url} isn't valid"
+    false
   end
 end
