4a41767b0c814113319ea3b394144c64d112b13c
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2019-09-15 19:19:30 +0300

    Implemented PostsController rooting specs.

---
+++ spec/routing/posts_routing_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/posts').to route_to('posts#index')
+    end
+  end
+end
