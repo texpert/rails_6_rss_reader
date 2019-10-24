91a2b8740b6d11db9b7045fe53448a31af144b01
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2019-10-24 14:45:06 +0300

    Bumped JS packages.
    Disabled actioncable and activestorage JS packages.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
+//require("@rails/activestorage").start()
 require("channels")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
