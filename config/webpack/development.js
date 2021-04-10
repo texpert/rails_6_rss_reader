3ab7138c6fb3c83d9e1a8eab276424b5af3fa3d7
texpert <branzeanu.aurel@gmail.com>
2021-04-10 23:38:40 +0300

    webpack-dashboard's port is used for communicating between the webpack-dashboard plugin and cli app via WebSockets, so it should be different from the webpack server's port

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,7 +5,7 @@ const webpackConfig = require('./base')
 const DashboardPlugin = require("webpack-dashboard/plugin")
 
 module.exports = {
-  plugins: [new DashboardPlugin({ port: 3035 })]
+  plugins: [new DashboardPlugin(3001)]
 }
 
 module.exports = merge(webpackConfig)
