03ac4fefee7e6274d036cc324af8dddfed8f97f0
texpert <branzeanu.aurel@gmail.com>
2021-04-10 23:21:05 +0300

    Add webpack-dashboard to package.json scripts
    When running `yarn dev` the webpack-dashboard is started, but only log window works so far

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+const { merge } = require('webpack-merge')
 const webpackConfig = require('./base')
+const DashboardPlugin = require("webpack-dashboard/plugin")
 
-module.exports = webpackConfig
+module.exports = {
+  plugins: [new DashboardPlugin({ port: 3035 })]
+}
+
+module.exports = merge(webpackConfig)
