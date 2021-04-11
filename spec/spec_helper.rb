b2949856ec6008593ab8fc829f892707f010aed8
texpert <branzeanu.aurel@gmail.com>
2021-04-12 02:03:14 +0300

    Made Capybara threadsafe and use :selenium_chrome as JS driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,3 +97,6 @@ RSpec.configure do |config|
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
 end
+
+Capybara.threadsafe = true
+Capybara.javascript_driver = :selenium_chrome
