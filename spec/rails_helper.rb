96f854ebdcf4e2875fe063b925518f24e2b20135
texpert <branzeanu.aurel@gmail.com>
2021-04-11 23:51:52 +0300

    Move `require 'capybara/rspec'` from rails_helper to spec_helper as recommended in the gem's docs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'capybara/rspec'
 require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
