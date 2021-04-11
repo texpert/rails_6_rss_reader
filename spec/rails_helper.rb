56a943b4c40dbf8d45c8eb0eb3a47c6b1fd1853f
texpert <branzeanu.aurel@gmail.com>
2021-04-12 02:10:57 +0300

    Fixed DatabaseCleaner config to use truncation strategy for feature specs and clean the DB after RSpecs `after` spec cleaning.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,30 +46,31 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation, except: %w[ar_internal_metadata])
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before do
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_spec_share_db = Capybara.current_driver == :rack_test
+
+    unless driver_spec_share_db
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
   end
 
   config.before do
     DatabaseCleaner.start
   end
 
-  config.after do
-    DatabaseCleaner.clean
-  end
-
-  config.before(:all) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:all) do
+  config.append_after do
     DatabaseCleaner.clean
   end
 
