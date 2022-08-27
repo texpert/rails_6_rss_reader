7b413373686fc3296f74b3e97bd6e3a276759706
texpert <branzeanu.aurel@gmail.com>
2022-08-27 22:48:02 +0300

    Use argument forwarding in LambdaPromoteJob

--- app/jobs/lambda_promote_job.rb
+++ app/jobs/lambda_promote_job.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class LambdaPromoteJob < Que::Job
-  def run(attacher_class, record_class, record_id, name, file_data)
+  def run(...)
     ActiveRecord::Base.transaction do
-      Shrine::Attacher.lambda_process(attacher_class, record_class, record_id, name, file_data)
+      Shrine::Attacher.lambda_process(...)
     end
   end
 end
