4745208e5ca93523ed257c54e744004b19f5edb9
texpert <branzeanu.aurel@gmail.com>
2023-05-29 14:02:54 +0300

    Remove from babel.config.js plugins included into babel/core

--- babel.config.js
+++ babel.config.js
@@ -37,12 +37,7 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
-      '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-transform-destructuring',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
       ['@babel/plugin-transform-runtime', { helpers: false, regenerator: true, corejs: false }],
       ['@babel/plugin-transform-regenerator', { async: false }]
     ].filter(Boolean)
